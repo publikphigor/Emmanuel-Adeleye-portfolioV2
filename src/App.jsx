fb0682a3a2151a53d8595e482ec2b83c0f43a7ed
Koladele Olaitan <koladeleolaitan@gmail.com>
2022-09-19 20:14:52 +0000

    Updated code docs.

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Loader } from "./components";
 import Home from "./pages/Home";
 
-// Define the App component, initialize state and pass down the theme state and setState function as a prop
-
+// Define the App component, initialize state and pass down the theme state and setState function as props
 function App() {
-  // Dark Mode Props
+  // Initialized dark mode state and set default theme to dark mode
   const [darkMode, setDarkMode] = useState(true);
   darkMode && document.documentElement.classList.add("dark");
 
